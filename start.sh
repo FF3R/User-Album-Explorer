0f39660b7a3b5f1e8e0d7ba5a3ae451fd8ef2491
Franco Feresini <fran.feresini@gmail.com>
2024-08-17 18:27:17 -0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,7 @@
+npx create-expo-app previo -t expo-template-blank         
+npm install axios      
+npm install @react-navigation/native @react-navigation/native-stack    
+npx expo install react-native-screens react-native-safe-area-context 
+npx expo install expo-secure-store               
+npm install react-native-paper
+npm install --save @react-native-community/netinfo
